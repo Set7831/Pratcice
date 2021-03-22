ddb03b9b056bc2f1a3beeef39fc446048f20f897
Andrey <strokin.andrei@mail.ru>
2021-03-22 22:20:32 +0300

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -1,57 +1,55 @@
-import './App.css';
+import React from 'react';
 import ListVehicle from './ListVehicle/ListVehicle.js';
 import ListVehicleData from './ListVehicleData.js';
+import './App.css';
 import SearchIcon from './search.svg';
 import AddIcon from './add.svg';
 
-function App() {
-  const list_vehicle = ListVehicleData.map(item =>{
-    return(
-      <ListVehicle
-        key={item.id}
-        ID={item.id}
-        Vehicle={item.vehicle}
-        Driver={item.driver}
-        Color={item.color}
-        Device={item.device}
-        Location={item.location}
-        Total_ODO={item.total_odo}
-        Edit={item.edit}
-        Delete={item.delete}
-      />
-    )
-  })
-  return (
+const App = () => (
     <div className="App">
       <div className="wrapper">
         <div className="navbar">
               <div className="search-container">
-                  <img src={SearchIcon} className="search-icon"/>
+                  <img src={SearchIcon} className="search-icon" alt="Search"/>
                   <input type="text" className="search-input" placeholder="Search" />
               </div>
               <div className="add-button">
                   <div className="add-icon-container">
-                    <img src={AddIcon} />
+                    <img src={AddIcon} className="add-icon" alt="Add Vehicle" />
                   </div>
                   <div className="button-text">Add new Vehicle</div>
               </div>
         </div>
-        <table className="table">
+        <table className="table" >
+          
               <tr className="table-row">
-                  <th>ID</th>
-                  <th>Vehicle</th>
+                  <th style={{width: "5%"}}>ID</th>
+                  <th style={{width: "15%"}}>Vehicle</th>
                   <th>Driver</th>
                   <th>Color</th>
                   <th>Device</th>
                   <th>Location</th>
                   <th>Total ODO</th>
-                  <th>Actions</th>
+                  <th style={{width: "5%"}}>Actions</th>
               </tr>
+              {ListVehicleData.map(item => (
+                <ListVehicle
+                  key={item.id}
+                  ID={item.id}
+                  Vehicle={item.vehicle}
+                  Driver={item.driver}
+                  Color={item.color}
+                  Device={item.device}
+                  Location={item.location}
+                  Total_ODO={item.total_odo}
+                  Edit={item.edit}
+                  Delete={item.delete}
+                />
+              ))}
           </table>
-        {list_vehicle}
+        
       </div>
     </div>
-  );
-}
+);
 
 export default App;
--- src/ListVehicle/ListVehicle.js
+++ src/ListVehicle/ListVehicle.js
@@ -1,32 +1,28 @@
-import './ListVehicle.css';
 import React from 'react';
+import './ListVehicle.css';
 import EditIcons from './edit.svg';
 import DeleteIcons from './delete.svg';
 
-const ListVehicle = props => {
-    return (
-        <table className="item-list">
-            <tr className="table-row">
-                <td>{props.ID}</td>
-                <td>{props.Vehicle}</td>
-                <td>{props.Driver}</td>
-                <td>{props.Color}</td>
-                <td>{props.Device}</td>
-                <td>{props.Location}</td>
-                <td>{props.Total_ODO}</td>
-                <td className="actions">
-                    <iconbutton className="edit" button_edit={props.edit}>
-                        <img src={EditIcons}/>
-                    </iconbutton>
-                    <iconbutton className="delete" button_delete={props.delete}>
-                    <img src={DeleteIcons}/>
-
-                    </iconbutton></td>
-            </tr>
-        </table>
-         
-    );
-}
+const ListVehicle = props => (
+        
+    <tr className="table-row">
+        <td style={{width: "5%"}}>{props.ID}</td>
+        <td style={{width: "15%"}}>{props.Vehicle}</td>
+        <td>{props.Driver}</td>
+        <td>{props.Color}</td>
+        <td>{props.Device}</td>
+        <td>{props.Location}</td>
+        <td>{props.Total_ODO}</td>
+        <td className="actions" style={{width: "5%"}}>
+            <iconbutton className="edit" buttonEdit={props.edit}>
+                <img src={EditIcons} alt="Edit"/>
+            </iconbutton>
+            <iconbutton className="delete" buttonDelete={props.delete}>
+                <img src={DeleteIcons} alt="Delete"/>
+            </iconbutton>
+        </td>
+    </tr> 
+);
 
 
 export default ListVehicle;
